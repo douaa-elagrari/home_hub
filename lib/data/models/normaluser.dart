19d9716609ea05b248a1ea7d91452010f2238069
kalimeriem <154741612+kalimeriem@users.noreply.github.com>
2025-12-06 21:21:23 +0000

    Update normaluser.dart

--- lib/data/models/normaluser.dart
+++ lib/data/models/normaluser.dart
@@ -13,27 +13,29 @@ class NormalUserModel extends UserModel {
     required this.fullName,
     required this.birthDate,
     required this.address,
+    String? profilePicture,
   }) : super(
          id: id,
          username: username,
          email: email,
          password: password,
          userType: "normal",
+         profilePicture: profilePicture,
        );
 
   @override
   Map<String, dynamic> toMap() {
     return {
-      // Use the EXACT column names from your DatabaseConfig!
-      // Don't include "id" - it's auto-increment
-      "type": userType, // Changed from "userType" to "type"
-      "name": username, // Changed from "username" to "name"
-      "email": email, // ✓ This one is correct
-      "password": password, // ✓ This one is correct
-      "fname": fullName, // Changed from "fullName" to "fname"
-      "bd": birthDate, // Changed from "birthDate" to "bd"
-      "addr": address, // Changed from "address" to "addr"
-      // Don't include "cname" (companyName) for normal users
+  
+      "type": userType, 
+      "name": username, 
+      "email": email, 
+      "password": password,
+      "fname": fullName, 
+      "bd": birthDate,
+      "addr": address, 
+
     };
   }
 }
+
