9b0335cfd52e44408bb0f7e9bd32b394fd4804c7
kalimeriem <154741612+kalimeriem@users.noreply.github.com>
2025-12-06 21:24:21 +0000

    Update app_localizations_fr.dart

--- lib/l10n/app_localizations_fr.dart
+++ lib/l10n/app_localizations_fr.dart
@@ -64,5 +64,130 @@ class AppLocalizationsFr extends AppLocalizations {
   String get products => "Produits";
   @override
   String get agencies => "Agences";
-  
+  @override
+  String get loggedInSuccess => 'Connexion réussie !';
+  @override
+  String get registeredSuccess => 'Inscription réussie !';
+
+  @override
+  String get signIn => 'Connexion';
+  @override
+  String get signUp => 'Inscription';
+  @override
+  String get usernameOrEmail => 'Nom d’utilisateur ou e-mail';
+  @override
+  String get enterUsernameOrEmail => 'Entrez le nom d’utilisateur ou l’e-mail';
+  @override
+  String get password => 'Mot de passe';
+  @override
+  String get enterPassword => 'Entrez le mot de passe';
+  @override
+  String get forgotPassword => 'Mot de passe oublié ?';
+  @override
+  String get dontHaveAccount => 'Vous n’avez pas de compte ?';
+  @override
+  String get alreadyHaveAccount => 'Vous avez déjà un compte ?';
+
+  @override
+  String get professionalSignUp => 'Inscription professionnelle';
+  @override
+  String get individual => 'Individuel';
+  @override
+  String get company => 'Entreprise';
+  @override
+  String get fullName => 'Nom complet';
+  @override
+  String get enterFullName => 'Entrez votre nom complet';
+  @override
+  String get companyName => 'Nom de l’entreprise';
+  @override
+  String get enterCompanyName => 'Entrez le nom de l’entreprise';
+  @override
+  String get phoneNumber => 'Numéro de téléphone';
+  @override
+  String get enterPhoneNumber => 'Entrez le numéro de téléphone';
+  @override
+  String get invalidPhoneNumber => 'Numéro de téléphone invalide';
+  @override
+  String get email => 'E-mail';
+  @override
+  String get enterEmail => 'Entrez l’e-mail';
+  @override
+  String get invalidEmail => 'E-mail invalide';
+  @override
+  String get vatNumber => 'Numéro TVA / d’enregistrement';
+  @override
+  String get enterVatNumber => 'Entrez le numéro TVA / d’enregistrement';
+  @override
+  String get location => 'Localisation';
+  @override
+  String get enterLocation => 'Entrez la localisation';
+  @override
+  String get description => 'Description';
+  @override
+  String get enterDescription => 'Entrez la description';
+  @override
+  String get atLeast6Chars => 'Au moins 6 caractères';
+  @override
+  String get confirmPassword => 'Confirmez le mot de passe';
+  @override
+  String get confirmYourPassword => 'Confirmez votre mot de passe';
+  @override
+  String get passwordsDoNotMatch => 'Les mots de passe ne correspondent pas';
+  @override
+  String get submitting => 'Envoi en cours...';
+
+  @override
+  String get growBusiness => 'Développez votre activité';
+  @override
+  String get growBusinessDesc =>
+      'Obtenez des clients et développez votre réputation professionnelle\nen ligne.';
+  @override
+  String get furnishStyleEasily => 'Aménagez & stylisez facilement';
+  @override
+  String get furnishStyleDesc =>
+      'Parcourez matériaux, meubles et idées de \ndesign adaptés à votre style et à votre budget.';
+  @override
+  String get findTrustedProfessionals => 'Trouvez des professionnels fiables';
+  @override
+  String get findTrustedDesc =>
+      'Connectez-vous avec des constructeurs,\n électriciens,plombiers et nettoyeurs fiables \n— à tout moment,partout.';
+  @override
+  String get welcomeToHomeHub => 'Bienvenue sur HomeHub';
+  @override
+  String get homeHubSubtitle =>
+      'Votre plateforme tout-en-un pour \nconstruire,finir et entretenir votre maison en Algérie';
+  @override
+  String get hireService => 'Engager un service';
+  @override
+  String get provideService => 'Fournir un service';
+  @override
+  String get signInPro => 'Se connecter en tant que professionnel';
+
+  @override
+  String get enterUsername => "Entrez le nom d'utilisateur";
+  @override
+  String get myProfile => "Mon profil";
+
+  @override
+  String get editProfile => "Modifier le profil";
+
+  @override
+  String get notifications => "Notifications";
+
+  @override
+  String get switchToServiceProviderAccount =>
+      "Passer au compte prestataire \nde services";
+
+  @override
+  String get logout => "Se déconnecter";
+  @override
+  String get companyAccount => 'Compte Société';
+  @override
+  String get freelancerAccount => 'Compte Freelance';
+  @override
+  String get normalAccount => 'Compte Normal';
+  @override
+  String get userAccount => 'Compte Utilisateur';
 }
+
